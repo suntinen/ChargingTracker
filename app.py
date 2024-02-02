86002484833c08790fb37b87006c2219de341439
suntinen <157013061+suntinen@users.noreply.github.com>
2024-02-02 15:24:09 +0200

    UI changes
    Changed font and some colorings, small bug fixes

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+#from dotenv import load_dotenv
 
-
-
+#load_dotenv()
 app = Flask(__name__)
 app.secret_key = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
